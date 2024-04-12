54f7279367caaa2953855028ad6f2c5363a41334
IsmoilGiga <ismoilagzamovman@gmail.com>
2024-04-12 11:45:36 +0500

    Sigma4.2

---
+++ index.js
@@ -0,0 +1,9 @@
+let string = String(prompt("String typedagi malumot kriting"));
+let number = Number(prompt("Number typedagi malumot kriting"));
+let boolean = Boolean(prompt("Boolean typedagi malumot kriting"));
+
+ confirm("Hamma malumotlar to'g'rimi?");
+
+ alert("Sizning kiritgan " + string + " so'zi " + typeof string + " typega tegishli!");
+ alert("Sizning kiritgan " + number + " soni " + typeof number + " typega tegishli!");
+ alert("Sizning kiritgan " + boolean + " so'zi " + typeof boolean + " typega tegishli!");
